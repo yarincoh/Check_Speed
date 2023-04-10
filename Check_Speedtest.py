5e4dc9d34c57307c674cf2f346ab0886dac3b9c2
Yarin Cohen <yrcohen8@gmail.com>
2023-04-10 23:14:16 +0300

    Threading

--- Check_Speedtest.py
+++ Check_Speedtest.py
@@ -2,6 +2,9 @@ import psutil
 import speedtest
 import time
 import socket
+import keyboard
+import signal
+import threading
 
 # Set up variables to track network status and speed
 last_status = None
@@ -11,10 +14,11 @@ last_report_time = 0
 
 # Define a function to test the network speed and return the download speed in Mbps
 def test_speed(interface_name):
-    st = speedtest.Speedtest()
-    st.get_best_server()
-    download_speed = st.download() / 1000000
-    return download_speed
+    while True:
+        st = speedtest.Speedtest()
+        st.get_best_server()
+        download_speed = st.download() / 1000000
+        return download_speed
 
 
 # Define a function to output a report of network status and speed
@@ -37,7 +41,7 @@ def output_report():
         elif last_status and time.time() - last_report_time >= 30:
             print(f"{now}: Network reconnected")
     else:
-        print(f"{now}: Network connected")
+        print(f"{time.strftime('%Y-%m-%d %H:%M:%S')}: Network connected")
 
     if last_speed is not None and last_interface == interface_name:
         print(f"{now}: Network speed dropped to {test_speed(interface_name):.2f} Mbps" if test_speed(interface_name) < last_speed else f"{now}: Network speed stable at {test_speed(interface_name):.2f} Mbps")
@@ -48,14 +52,28 @@ def output_report():
     last_interface = interface_name
     last_report_time = time.time()
 
-# Output a report once every 24 hours
+# if ctrl+c is pressed the script stop
+def signal_handler(sig, frame):
+    print("Program terminated manually!")
+    raise SystemExit
+
+# Output a report non stop
 while True:
-    output_report()
-    for i in range(100):  # Output the network speed every 5 seconds (5 times per report)
+    t = threading.Thread(target=test_speed, args=(last_interface,))
+    t.daemon = True
+    t.start()
+    try:
+        output_report()
+    except KeyboardInterrupt: # if ctrl+c is pressed the script stop
+        print("Program terminated manually!")
+        raise SystemExit
+    for i in range(6):  # Output the network speed every 15 seconds 
         try:
             network_speed = test_speed(last_interface)
             print(f"{time.strftime('%Y-%m-%d %H:%M:%S')}: Network speed: {network_speed:.2f} Mbps")
         except Exception as e:
             print(f"{time.strftime('%Y-%m-%d %H:%M:%S')}: Error: {e}\nWe can't connect to the internet.")
-            time.sleep(5)
-        time.sleep(1)
+            time.sleep(2)
+        except KeyboardInterrupt: # if ctrl+c is pressed the script stop
+            signal.signal(signal.SIGINT, signal_handler)
+        time.sleep(3)
